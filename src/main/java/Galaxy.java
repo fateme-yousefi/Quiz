66dde352c10dd5ef18ae963899fe8f1d7310494e
Reyhane <r_arefy@yahoo.com>
2024-10-08 15:34:17 +0330

    Added Files

---
+++ src/main/java/Galaxy.java
@@ -0,0 +1,26 @@
+package src.main.java;
+
+import java.util.ArrayList;
+
+public class Galaxy {
+    private String name;
+    private ArrayList<Planet> planets;
+
+    public Galaxy(String name) {
+        this.name = name;
+        this.planets = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+
+    }
+
+    public ArrayList<Planet> getPlanets() {
+        return planets;
+    }
+
+    public void addPlanet(Planet planet) {
+        planets.add(planet);
+    }
+}
